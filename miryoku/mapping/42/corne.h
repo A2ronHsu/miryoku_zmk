4d2de039127205dda64c94b3c839d4f7235ab6c1
Aaron <aaronbee1990@gmail.com>
2024-08-24 10:39:14 -0400

    added pinky stagger key

--- miryoku/mapping/42/corne.h
+++ miryoku/mapping/42/corne.h
@@ -5,15 +5,24 @@
 
 #define XXX &none
 
+
+#define L1 &kp GRAVE
+#define L2 &kp EXCL
+#define L3 &kp BSLH
+#define R1 &kp EQUAL
+#define R2 &kp MINUS
+#define R3 &kp SEMICOLON
+
+
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
+L1  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  R1 \
+L2  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  R2 \
+L3  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  R3 \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
